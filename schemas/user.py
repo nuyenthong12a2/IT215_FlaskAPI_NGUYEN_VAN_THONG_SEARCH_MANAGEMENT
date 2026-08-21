a273e6b44da3c2a4bc76747cd6e1b35ba92d6252
Nguyen THong <nguyenthong12122015@gmail.com>
2026-08-21 23:15:24 +0700

    Fix : ID đưa lên đầu

--- schemas/user.py
+++ schemas/user.py
@@ -20,11 +20,13 @@ class UserUpdate(BaseModel):
         from_attributes = True
 
 
-class UserResponse(UserBase):
-    id: int
-    role: str
-    is_active: bool
-    created_at: datetime
 
+class UserResponse(BaseModel):
+    id:int 
+    email:EmailStr
+    full_name:str
+    role:str 
+    is_active:bool
+    created_at:datetime 
     class Config:
         from_attributes = True
